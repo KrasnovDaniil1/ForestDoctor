5c70cf591ab15078d6827384d447de506cbdb740
KrasnovDaniil1 <kd69931@gmail.com>
2025-03-05 16:00:36 +0200

    add

---
+++ cmd/run/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Readme (структура проекта)
+// .env
+// docker
+// логирование в файл
+// тестирование с помощью функций
+// swager
+
+
+func main() {
+    fmt.Println("Hello, World!")
+}
