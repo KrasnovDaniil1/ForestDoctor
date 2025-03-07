dacf240a756b2d1e9d30665db18ec8e439e9ba17
KrasnovDaniil1 <kd69931@gmail.com>
2025-03-07 22:21:24 +0200

    add

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 // Readme (структура проекта)
 // .env
@@ -9,7 +12,17 @@ import "fmt"
 // тестирование с помощью функций
 // swager
 
+// проверка на не существующий роутинг
+// проверка на sql инъекции ???
+// проверка на код ошибки
+
+// time_table - id name_tablet time period start end 
 
 func main() {
     fmt.Println("Hello, World!")
+    http.HandleFunc('/', index)
+
+    if err:= http.ListenAndServe(":8080", nil); err != nil {
+        //
+    }
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module forestdoctor
+
+go 1.22.2
